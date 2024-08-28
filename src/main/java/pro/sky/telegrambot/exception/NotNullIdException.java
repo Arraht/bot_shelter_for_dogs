e90afb059417829b14c954f70acfa1a0cc645ea2
manxix69 <manxix69@gmail.com>
2024-08-28 23:38:17 +0500

    создал архитектуру. для фронта по приютам. задокументировал класс ShelterServiceImpl и ShelterController

---
+++ src/main/java/pro/sky/telegrambot/exception/NotNullIdException.java
@@ -0,0 +1,11 @@
+package pro.sky.telegrambot.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class NotNullIdException extends RuntimeException {
+    public NotNullIdException(String string) {
+        super(string);
+    }
+}
--- src/main/resources/liquibase/scripts/bot.sql
+++ src/main/resources/liquibase/scripts/bot.sql
@@ -1,25 +1,51 @@
--- liquibase formated sql
+-- liquibase formatted sql
 
---changeset Igor:1
+-- changeset Igor:1
 CREATE TABLE volunteers(
 id SERIAL,
 name TEXT,
 nickName TEXT,
-year INTEGER,
 workingFirstTime TIMESTAMP,
 workingLastTime TIMESTAMP
-);
+)
+-- changeset Igor:2
 CREATE TABLE answer(
 id SERIAL,
 command TEXT,
 message TEXT
-);
+)
+-- changeset Igor:3
+CREATE TABLE Client(
+id SERIAL,
+name TEXT,
+chatId INTEGER
+)
+-- changeset Igor:4
 CREATE TABLE BotTalkClient(
 id SERIAL,
+answerId INTEGER,
 name TEXT,
 chatId INTEGER,
 timeSendMessage TIMESTAMP,
 initialSend TEXT,
-answerMessage TEXT,
 successOfSending BOOLEAN
+)
+-- changeset Igor:5
+CREATE TABLE VolunteersTalkClient(
+id SERIAL,
+nameVolunteers TEXT,
+nameClient TEXT,
+chatId INTEGER,
+appointmentTime TIMESTAMP
+)
+-- changeset Sergei:6
+CREATE TABLE BotTalkClient(
+id SERIAL,
+name TEXT,
+address TEXT,
+address TEXT,
+workSchedule TEXT,
+directionsMap BLOB,
+securityContact TEXT,
+generalRecommendationsOnSafety TEXT
 )
