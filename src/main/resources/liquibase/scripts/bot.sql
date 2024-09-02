0e3aeec0365c27da3431c9a08b1a00bf802828cd
manxix69 <manxix69@gmail.com>
2024-09-02 22:02:23 +0500

    обновил bot.sql для синхронизации

--- src/main/resources/liquibase/scripts/bot.sql
+++ src/main/resources/liquibase/scripts/bot.sql
@@ -12,20 +12,20 @@ workingLastTime TIMESTAMP
 CREATE TABLE answer(
 id SERIAL,
 command TEXT,
-message TEXT
+chat_id INTEGER
 )
 -- changeset Igor:3
 CREATE TABLE Client(
 id SERIAL,
 name TEXT,
-chatId INTEGER
+chat_id INTEGER
 )
 -- changeset Igor:4
 CREATE TABLE BotTalkClient(
 id SERIAL,
 answerId INTEGER,
 name TEXT,
-chatId INTEGER,
+chat_id INTEGER,
 timeSendMessage TIMESTAMP,
 initialSend TEXT,
 successOfSending BOOLEAN
@@ -35,7 +35,7 @@ CREATE TABLE VolunteersTalkClient(
 id SERIAL,
 nameVolunteers TEXT,
 nameClient TEXT,
-chatId INTEGER,
+chat_id INTEGER,
 appointmentTime TIMESTAMP
 )
 -- changeset Sergei:6
@@ -43,50 +43,24 @@ CREATE TABLE shelter(
 id SERIAL,
 name TEXT,
 address TEXT,
-workSchedule TEXT,
-directionsMap BYTEA,
-securityContact TEXT,
-generalRecommendationsOnSafety TEXT
+work_schedule TEXT,
+security_contact TEXT,
+general_recommendations_on_safety TEXT
 )
 -- changeset Sergei:7
 CREATE TABLE picture(
 id SERIAL,
-filePath TEXT,
-fileSize SERIAL,
-MediaType TEXT,
+file_path TEXT,
+file_size SERIAL,
+media_type TEXT,
 data BYTEA
 )
 -- changeset Sergei:8
 CREATE TABLE direction_to_shelter_picture(
-shelter SERIAL
+shelter_id SERIAL
 ) INHERITS (picture);
 -- changeset Sergei:9
-ALTER TABLE shelter RENAME COLUMN directionsmap TO directionspicture;
--- changeset Sergei:10
-ALTER TABLE shelter ALTER COLUMN directionspicture TYPE TEXT;
--- changeset Sergei:11
 ALTER TABLE picture add primary key(id)
--- changeset Sergei:12
-ALTER TABLE shelter ADD CONSTRAINT directionspicture_constraint foreign key (id) references picture
--- changeset Sergei:13
-ALTER TABLE shelter RENAME COLUMN directionspicture TO directions_picture_id;
--- changeset Sergei:14
-ALTER TABLE shelter RENAME COLUMN generalrecommendationsonsafety TO general_recommendations_on_safety;
--- changeset Sergei:15
-ALTER TABLE shelter RENAME COLUMN securitycontact TO security_contact;
--- changeset Sergei:16
-ALTER TABLE shelter RENAME COLUMN workschedule TO work_schedule;
--- changeset Sergei:17
-ALTER TABLE shelter DROP CONSTRAINT directionspicture_constraint;
--- changeset Sergei:18
+-- changeset Sergei:10
 ALTER TABLE shelter add primary key(id)
--- changeset Sergei:19
-ALTER TABLE shelter DROP COLUMN directions_picture_id;
--- changeset Sergei:20
-ALTER TABLE Direction_To_Shelter_Picture RENAME COLUMN shelter TO shelter_id;
--- changeset Sergei:21
-ALTER TABLE picture RENAME COLUMN mediatype TO media_type;
--- changeset Sergei:22
-ALTER TABLE picture RENAME COLUMN filepath TO file_path;
--- changeset Sergei:23
-ALTER TABLE picture RENAME COLUMN filesize TO file_size;
+
