0762900bbb50079248e22020d3790282942000d0
manxix69 <manxix69@gmail.com>
2024-09-06 22:22:53 +0500

    добавил второй тест

--- src/test/java/pro/sky/telegrambot/service/shelter/ShelterServiceImplTests.java
+++ src/test/java/pro/sky/telegrambot/service/shelter/ShelterServiceImplTests.java
@@ -9,6 +9,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import pro.sky.telegrambot.entity.Shelter;
 import pro.sky.telegrambot.exception.NotFoundShelterByIdException;
 import pro.sky.telegrambot.interfaces.shelter.ShelterService;
 import pro.sky.telegrambot.repository.shelter.ShelterRepository;
@@ -23,11 +24,23 @@ public class ShelterServiceImplTests {
     @MockBean
     private ShelterRepository shelterRepository;
 
+    private final Shelter TEST_SHELTER = new Shelter();
+
+
     @Test
     public void shouldBeReturnNotFoundShelterByIdException() {
         Mockito.when(shelterRepository.findById(-1l)).thenReturn(Optional.ofNullable(null));
-
         Assertions.assertThrows(NotFoundShelterByIdException.class, ()->shelterService.findById(-1l));
+    }
+
+    @Test
+    public void shouldBeReturnShelter() {
+        TEST_SHELTER.setId(1l);
+        Shelter shelter;
+        Mockito.when(shelterRepository.findById(TEST_SHELTER.getId())).thenReturn(Optional.ofNullable(TEST_SHELTER));
+        shelter = shelterService.findById(TEST_SHELTER.getId());
+
+        Assertions.assertEquals(shelter, TEST_SHELTER);
     }
 
 }
