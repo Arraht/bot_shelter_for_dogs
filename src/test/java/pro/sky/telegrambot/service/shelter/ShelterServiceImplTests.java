f881b2240b1d6c0ccf2bad1bb509cabb106d87f6
manxix69 <manxix69@gmail.com>
2024-09-07 11:07:35 +0500

    покрыл тестами ShelterServiceImplTests и добавил в него комментарии

--- src/test/java/pro/sky/telegrambot/service/shelter/ShelterServiceImplTests.java
+++ src/test/java/pro/sky/telegrambot/service/shelter/ShelterServiceImplTests.java
@@ -11,10 +11,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import pro.sky.telegrambot.entity.Shelter;
 import pro.sky.telegrambot.exception.NotFoundShelterByIdException;
+import pro.sky.telegrambot.exception.NotNullIdException;
 import pro.sky.telegrambot.interfaces.shelter.ShelterService;
 import pro.sky.telegrambot.repository.shelter.ShelterRepository;
 
 import java.util.Optional;
+
 
 @SpringBootTest
 @ExtendWith(MockitoExtension.class)
@@ -25,8 +27,21 @@ public class ShelterServiceImplTests {
     private ShelterRepository shelterRepository;
 
     private final Shelter TEST_SHELTER = new Shelter();
+    private final Shelter TEST_SHELTER_WITH_NULL_ID = new Shelter();
 
+    private Shelter ACTUAL_SHELTER = new Shelter();
+    private Shelter EXPEPTED_SHELTER = new Shelter();
 
+    @BeforeEach
+    void init_shelter(){
+        TEST_SHELTER.setId(1l);
+
+    }
+
+    /**
+     * проверяет возрвщается ли ошибка если не найден приют в БД
+     * @Note для тестирования используется Mockito
+     */
     @Test
     public void shouldBeReturnNotFoundShelterByIdException() {
         Mockito.when(shelterRepository.findById(-1l)).thenReturn(Optional.ofNullable(null));
@@ -34,14 +49,85 @@ public class ShelterServiceImplTests {
         Assertions.assertThrows(NotFoundShelterByIdException.class, ()->shelterService.findById(-1l));
     }
 
+    /**
+     * проверяет совпадает ли идентификатор по id у приютов при поиске в методе
+     * @Note для тестирования используется Mockito
+     */
     @Test
-    public void shouldBeReturnShelter() {
-        TEST_SHELTER.setId(1l);
-        Shelter shelter;
+    public void shouldBeАFindByIdShelter() {
         Mockito.when(shelterRepository.findById(TEST_SHELTER.getId())).thenReturn(Optional.ofNullable(TEST_SHELTER));
-        shelter = shelterService.findById(TEST_SHELTER.getId());
+        EXPEPTED_SHELTER = shelterService.findById(TEST_SHELTER.getId());
+        Assertions.assertEquals(EXPEPTED_SHELTER, TEST_SHELTER);
+    }
 
-        Assertions.assertEquals(shelter, TEST_SHELTER);
+    /**
+     * добавляет приют (без id)
+     * @Note для тестирования используется Mockito
+     */
+    @Test
+    public void shouldBeAddShelter() {
+        Mockito.when(shelterRepository.save(TEST_SHELTER_WITH_NULL_ID)).thenReturn(TEST_SHELTER);
+        EXPEPTED_SHELTER = shelterService.add(TEST_SHELTER_WITH_NULL_ID);
+        Assertions.assertEquals(EXPEPTED_SHELTER, TEST_SHELTER);
+    }
+
+    /**
+     * попытка добавить приют у которого уже присвоен id
+     */
+    @Test
+    public void addShelterShouldBeReturnNotNullIdException() {
+        Assertions.assertThrows(NotNullIdException.class, ()-> shelterService.add(TEST_SHELTER));
+    }
+
+    /**
+     * поиск приюта - по id
+     * @Note для тестирования используется Mockito
+     */
+    @Test
+    public void shouldBeFindShelter() {
+        Mockito.when(shelterRepository.findById(TEST_SHELTER.getId())).thenReturn(Optional.of(TEST_SHELTER));
+        EXPEPTED_SHELTER = shelterService.find(TEST_SHELTER);
+        Assertions.assertEquals(EXPEPTED_SHELTER, TEST_SHELTER);
+    }
+
+    /**
+     * поиск приюта - по id
+     * @Note для тестирования используется Mockito
+     */
+    @Test
+    public void tryFindShelterShouldBeReturnNotFoundShelterByIdException() {
+        Mockito.when(shelterRepository.findById(TEST_SHELTER.getId())).thenReturn(Optional.empty());
+        Assertions.assertThrows(NotFoundShelterByIdException.class, ()-> shelterService.find(TEST_SHELTER));
+    }
+
+    /**
+     * поиск приюта - по id и редактирвоание данных на примере изменения имени
+     * @Note для тестирования используется Mockito
+     */
+    @Test
+    public void shouldBeEditShelter() {
+        EXPEPTED_SHELTER.setName("TEST NAME MUST CHANGE");
+        TEST_SHELTER.setName("NAME WAS CHANGED");
+
+        Mockito.when(shelterRepository.findById(TEST_SHELTER.getId())).thenReturn(Optional.of(TEST_SHELTER));
+        Mockito.when(shelterRepository.save(TEST_SHELTER)).thenReturn(TEST_SHELTER);
+
+        EXPEPTED_SHELTER = shelterService.edit(TEST_SHELTER);
+
+        Assertions.assertEquals(EXPEPTED_SHELTER, TEST_SHELTER);
+        Assertions.assertEquals(EXPEPTED_SHELTER.getName(), TEST_SHELTER.getName());
+    }
+
+    /**
+     * удаление приюта - по id
+     * @Note для тестирования используется Mockito
+     */
+    @Test
+    public void shouldBeRemoveShelter() {
+        TEST_SHELTER.setId(1L);
+        Mockito.when(shelterRepository.findById(TEST_SHELTER.getId())).thenReturn(Optional.of(TEST_SHELTER));
+        EXPEPTED_SHELTER = shelterService.remove(TEST_SHELTER);
+        Assertions.assertEquals(EXPEPTED_SHELTER, TEST_SHELTER);
     }
 
 }
